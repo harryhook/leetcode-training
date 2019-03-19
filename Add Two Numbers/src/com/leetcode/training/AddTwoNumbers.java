542e93c44a1beeab2d17960a05e8265ee121384b
chenhaowei <chenhaowei002@ke.com>
2019-03-19 18:09:59 +0800

    update md

---
+++ Add Two Numbers/src/com/leetcode/training/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.leetcode.training;
+
+
+/**
+ * @author chenhaowei
+ * @date 2019.03.19
+ */
+public class AddTwoNumbers {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
