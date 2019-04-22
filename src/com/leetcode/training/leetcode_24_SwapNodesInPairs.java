a8aff85aa036ca7526539e3df053d617c1857171
HarryHook <chenhaowei93@163.com>
2019-04-22 23:49:06 +0800

    commit swap pairs nodes

---
+++ src/com/leetcode/training/leetcode_24_SwapNodesInPairs.java
@@ -0,0 +1,41 @@
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode second = head.next;
+        ListNode third = second.next;
+        second.next = head;
+        head.next = swapPairs(third);
+        return second;
+    }
+
+    public ListNode swapPairs1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        while (cur.next != null || cur.next.next != null) {
+            ListNode first = cur.next;
+            ListNode second = first.next;
+            first.next = second.next;
+            second.next = first;
+            cur.next = second;
+            cur = first;
+        }
+        return dummyHead.next;
+    }
+}
