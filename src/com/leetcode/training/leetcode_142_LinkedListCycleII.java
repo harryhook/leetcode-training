ad86b1ed2e20c11a1c5a5144fd9f490ef0cd4090
chenhaowei <chenhaowei002@ke.com>
2019-04-25 12:26:05 +0800

    update linked list cycle ii

---
+++ src/com/leetcode/training/leetcode_142_LinkedListCycleII.java
@@ -0,0 +1,31 @@
+package com.leetcode.training;
+
+/**
+ * @author chenhaowei
+ * @date 2019.04.24
+ */
+public class leetcode_142_LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (slow != fast) {
+            return null;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
