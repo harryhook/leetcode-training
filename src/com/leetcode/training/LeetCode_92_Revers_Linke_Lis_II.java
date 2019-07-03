05ac14eaf94fa3c1adb6ef395dc81ebe9a36b8ab
harryhook <chenhaowei93@163.com>
2019-07-03 21:52:36 +0800

    LeetCode 92 :fire:

---
+++ src/com/leetcode/training/LeetCode_92_Revers_Linke_Lis_II.java
@@ -0,0 +1,21 @@
+package com.leetcode.training;
+
+public class LeetCode_92_Revers_Linke_Lis_II {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode pre = dummyHead;
+        for (int i = 0; i < m - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode tail = pre.next;
+        ListNode temp;
+        for (int i = 0; i < n - m; i++) {
+            temp = pre.next;
+            pre.next = tail.next;
+            tail.next = tail.next.next;
+            pre.next.next = temp;
+        }
+        return dummyHead.next;
+    }
+}
