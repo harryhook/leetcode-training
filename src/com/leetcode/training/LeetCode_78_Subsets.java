d721429781d3419e12c6299ab73705632d5108a1
chenhaowei <chenhaowei002@ke.com>
2020-07-26 19:35:30 +0800

    backtrack

---
+++ src/com/leetcode/training/LeetCode_78_Subsets.java
@@ -0,0 +1,39 @@
+package com.leetcode.training;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chenhaowei
+ * @date 2020.07.26
+ */
+public class LeetCode_78_Subsets {
+
+    public static void main(String[] args) {
+
+        LeetCode_78_Subsets client = new LeetCode_78_Subsets();
+        int[] nums = {1,2,3};
+
+        System.out.println(client.subsets(nums));
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        backTrack(res, nums, new ArrayList<>(), 0);
+
+        return res;
+    }
+
+    private void backTrack(List<List<Integer>> res, int[] nums, ArrayList<Integer> temp, int start) {
+
+        res.add(new ArrayList<>(temp));
+
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backTrack(res, nums, temp, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
