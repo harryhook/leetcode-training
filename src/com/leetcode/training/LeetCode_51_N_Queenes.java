53fddaa5a65d990a99b6e8b59ab01884f5bd4532
chenhaowei002 <chenhaowei002@ke.com>
2020-08-07 18:57:52 +0800

    leetcode 50

---
+++ src/com/leetcode/training/LeetCode_51_N_Queenes.java
@@ -0,0 +1,20 @@
+package com.leetcode.training;
+
+import java.util.List;
+
+/**
+ * @author chenhaowei
+ * @date 2020.08.06
+ */
+public class LeetCode_51_N_Queenes {
+
+    public static void main(String[] args) {
+        LeetCode_51_N_Queenes client = new LeetCode_51_N_Queenes();
+        client.nQueue(5);
+    }
+
+    public List<List<String>> nQueue(int n) {
+
+        return null;
+    }
+}
