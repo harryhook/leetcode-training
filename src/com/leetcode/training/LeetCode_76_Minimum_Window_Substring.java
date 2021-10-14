b53edb25b61f18d6eb560b6e40c0324e5619c187
harryhook <chenhaowei002.com>
2021-10-14 23:56:42 +0800

    Update LeetCode_76_Minimum_Window_Substring.java

--- src/com/leetcode/training/LeetCode_76_Minimum_Window_Substring.java
+++ src/com/leetcode/training/LeetCode_76_Minimum_Window_Substring.java
@@ -8,7 +8,8 @@ public class LeetCode_76_Minimum_Window_Substring {
     public static void main(String[] args) {
         LeetCode_76_Minimum_Window_Substring client = new LeetCode_76_Minimum_Window_Substring();
 
-        System.out.println(client.minWindow("ADBANC", "ABC"));;
+        System.out.println(client.minWindow("ADBANC", "ABC"));
+        ;
     }
 
 
@@ -18,29 +19,36 @@ public class LeetCode_76_Minimum_Window_Substring {
 
         Map<Character, Integer> map = new HashMap<>();
 
-        int left = 0, cnt = 0, minLen = Integer.MAX_VALUE;
-
+        int left = 0, cnt = 0, minLeft = 0, minLen = Integer.MAX_VALUE;
         String res = "";
         for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if (map.get(curr) != null && map.get(curr) >= 0) {
-                cnt++;
+        for (int right = 0; right < s.length(); right++) {
+            char curr = s.charAt(right);
+            if (map.get(curr) != null) {
+                if (map.get(curr) > 0) {
+                    cnt++;
+                }
                 map.put(curr, map.get(curr) - 1);
+
             }
 
             while (cnt == t.length()) {
-                if (minLen > i - left + 1) {
-                    minLen = i - left + 1;
-                    res = s.substring(left, minLen);
+                if (minLen > right - left + 1) {
+                    minLeft = left;
+                    minLen = right - left + 1;
+
+                    res = s.substring(left, minLeft + minLen);
                 }
 
-                if (map.get(curr) != null && map.get(curr) > 0) {
-                    map.put(curr, map.get(s.charAt(curr)) + 1);
-                    cnt--;
+                char leftChar = s.charAt(left);
+                if (map.get(leftChar) != null) {
+                    if (map.get(leftChar) >= 0) {
+                        cnt--;
+                    }
+                    map.put(leftChar, map.get(leftChar) + 1);
                 }
                 left++;
             }
