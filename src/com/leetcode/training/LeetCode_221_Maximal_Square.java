a687901c76cd9f041015d9e742cb0484e8df21dd
harryhook <chenhaowei93@163.com>
2021-12-15 10:42:36 +0800

    max square

---
+++ src/com/leetcode/training/LeetCode_221_Maximal_Square.java
@@ -0,0 +1,28 @@
+package com.leetcode.training;
+
+public class LeetCode_221_Maximal_Square {
+
+    public static void main(String[] args) {
+        char[][] matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+
+        LeetCode_221_Maximal_Square client = new LeetCode_221_Maximal_Square();
+        System.out.println(client.maximalSquare(matrix));
+    }
+    public int maximalSquare(char[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m + 1][n + 1];
+        int result = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i-1][j-1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    result = Math.max(result, dp[i][j]);
+                }
+            }
+        }
+        return result*result;
+    }
+}
