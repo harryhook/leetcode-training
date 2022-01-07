39071d2412307e7681ca76b912a8124e33c0c888
harryhook <chenhaowei002.com>
2022-01-07 23:36:13 +0800

    robber2

--- src/com/leetcode/training/LeetCode_198_House_Robber.java
+++ src/com/leetcode/training/LeetCode_198_House_Robber.java
@@ -22,14 +22,14 @@ public class LeetCode_198_House_Robber {
     public int rob1(int[] nums, int start, int end ) {
 
         if(nums.length == 1) return nums[0];
-        int []dp = new int[nums.length-1];
-        dp[0] = nums[start];
-        dp[1] = Math.max(nums[start], nums[start+1]);
+        int []dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start+1] = Math.max(nums[start], nums[start+1]);
 
-        for(int i=2; i<end-start; i++) {
+        for(int i=start+2; i<end; i++) {
             dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
         }
-        return dp[nums.length-2];
+        return dp[end-1];
     }
 
     public static void main(String[] args) {
