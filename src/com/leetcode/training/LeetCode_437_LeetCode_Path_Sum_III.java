407347b3c59281a211c977f41d587c36b0daa3e5
harryhook <chenhaowei002.com>
2022-02-17 00:11:27 +0800

    pre sum

--- src/com/leetcode/training/LeetCode_437_LeetCode_Path_Sum_III.java
+++ src/com/leetcode/training/LeetCode_437_LeetCode_Path_Sum_III.java
@@ -1,5 +1,7 @@
 package com.leetcode.training;
 
+import java.util.HashMap;
+
 public class LeetCode_437_LeetCode_Path_Sum_III {
 
     private  int count = 0;
@@ -33,7 +35,7 @@ public class LeetCode_437_LeetCode_Path_Sum_III {
         left22.right = right33;
 
         LeetCode_437_LeetCode_Path_Sum_III client = new LeetCode_437_LeetCode_Path_Sum_III();
-        System.out.println( client.pathSum(root, 18));
+        System.out.println( client.pathSum1(root, 8));
 
 
     }
@@ -62,5 +64,33 @@ public class LeetCode_437_LeetCode_Path_Sum_III {
 
         preOrder(root.left , subSum);
         preOrder(root.right, subSum);
+    }
+
+    public int pathSum1(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        helper(root, 0, sum, preSum);
+        return count;
+    }
+    public void helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return;
+        }
+
+        currSum += root.val;
+
+        if (preSum.containsKey(currSum - target)) {
+            count += preSum.get(currSum - target);
+        }
+
+        if (!preSum.containsKey(currSum)) {
+            preSum.put(currSum, 1);
+        } else {
+            preSum.put(currSum, preSum.get(currSum)+1);
+        }
+
+        helper(root.left, currSum, target, preSum);
+        helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
     }
 }
