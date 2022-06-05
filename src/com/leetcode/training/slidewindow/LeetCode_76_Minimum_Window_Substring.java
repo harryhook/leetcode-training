6cb093e2f6a06c749c9fe5a93d71bd7306c87308
harryhook <chenhaowei93@163.com>
2022-06-05 23:33:13 +0800

    滑动窗口

--- src/com/leetcode/training/slidewindow/LeetCode_76_Minimum_Window_Substring.java
+++ src/com/leetcode/training/slidewindow/LeetCode_76_Minimum_Window_Substring.java
@@ -1,4 +1,4 @@
-package com.leetcode.training;
+package com.leetcode.training.slidewindow;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,10 +8,8 @@ public class LeetCode_76_Minimum_Window_Substring {
     public static void main(String[] args) {
         LeetCode_76_Minimum_Window_Substring client = new LeetCode_76_Minimum_Window_Substring();
 
-        char c = 0;
-        System.out.println(--c);
-        System.out.println(client.minWindow("ADBANC", "ABC"));
-        ;
+        System.out.println(client.minWindow("EBBANCF", "ABC"));
+
     }
 
 
@@ -33,8 +31,6 @@ public class LeetCode_76_Minimum_Window_Substring {
                 if (map.get(curr) > 0) {
                     cnt++;
                 }
-                map.put(curr, map.get(curr) - 1);
-
             }
 
             while (cnt == t.length()) {
@@ -52,7 +48,6 @@ public class LeetCode_76_Minimum_Window_Substring {
                     }
                     map.put(leftChar, map.get(leftChar) + 1);
                 }
-                left++;
             }
         }
 
@@ -63,37 +58,46 @@ public class LeetCode_76_Minimum_Window_Substring {
 
         if (s == null || t == null || s.isEmpty() || t.isEmpty()) return "";
 
-       int[] maps = new int[128];
+        Map<Character, Integer> needMmap = new HashMap<>();
+        Map<Character, Integer> windowMap = new HashMap<>();
 
-        int left = 0, cnt = 0, minLeft = 0, minLen = Integer.MAX_VALUE;
-        String res = "";
+        int left = 0, right = 0, cnt = 0, minLeft = 0, minLen = Integer.MAX_VALUE;
+        // 统计t中字符出现的次数
         for (char c : t.toCharArray()) {
-            maps[c]++;
+            needMmap.put(c, needMmap.getOrDefault(c, 0) + 1);
         }
 
-        for (int right = 0; right < s.length(); right++) {
+        while (right < s.length()) {
             char curr = s.charAt(right);
-            if (--maps[curr] >= 0) {
-                cnt++;
+            right++;
+            if (needMmap.get(curr) != null) {
+                // 更新window
+                windowMap.put(curr, windowMap.getOrDefault(curr, 0) + 1);
+                if (needMmap.get(curr).equals(windowMap.get(curr))) {
+                    cnt++;
+                }
             }
-
+            // 左侧窗口是否需要收缩
             while (cnt == t.length()) {
-                if (minLen > right - left + 1) {
+                if (minLen > right - left) {
                     minLeft = left;
-                    minLen = right - left + 1;
-
-                    res = s.substring(left, minLeft + minLen);
+                    minLen = right - left;
                 }
 
                 char leftChar = s.charAt(left);
-                if (++maps[leftChar] > 0) {
-                        cnt--;
-                }
                 left++;
+                // 进行窗口的下一次调整
+                if (needMmap.get(leftChar) != null) {
+                    if (needMmap.get(leftChar).equals(windowMap.get(leftChar))) {
+                        cnt--;
+                    }
+                    windowMap.put(leftChar, windowMap.get(leftChar) - 1);
+                }
             }
         }
 
-        return res;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
     }
+
 
 }
