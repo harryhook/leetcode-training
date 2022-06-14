e7551169c62583daca9cd96f6abeb4ce2cd4402d
harryhook <chenhaowei002@ke.com>
2022-06-14 18:58:45 +0800

    交集

--- src/com/leetcode/training/interval/LeetCode_986_Interval_List_Intersections.java
+++ src/com/leetcode/training/interval/LeetCode_986_Interval_List_Intersections.java
@@ -26,20 +26,22 @@ public class LeetCode_986_Interval_List_Intersections {
             int[] a = firstList[i];
             int[] b = secondList[j];
 
-            if (a[0] <= b[1]) {
+
+            if (a[0] <= b[1] && b[0] <= a[1]) {
                 int[] temp = new int[]{Math.max(a[0], b[0]), Math.min(a[1], b[1])};
                 res.add(temp);
-                i++;
-            } else if (b[0] <= a[1]) {
-                int[] temp = new int[]{Math.max(a[0], b[0]), Math.min(a[1], b[1])};
-                res.add(temp);
-                j++;
-            } else if (a[1] < b[1]) {
+                if(a[0] < b[1]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            } else  if (a[1] < b[1]) {
                 i++;
             } else if (a[1] > b[1]) {
                 j++;
             }
         }
+
         int[][] resArr = new int[res.size()][2];
         for (int k = 0; k < res.size(); k++) {
             resArr[k][0] = res.get(k)[0];
