3cf915f1f7f92fe1234e02272b14e63d0ee96c3f
harryhook <chenhaowei08@meituan.com>
2026-07-05 18:32:29 +0800

    136

---
+++ src/com/leetcode/training/LeetCode_136_SingleNumber.java
@@ -0,0 +1,13 @@
+package com.leetcode.training;
+
+public class LeetCode_136_SingleNumber {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int res = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
